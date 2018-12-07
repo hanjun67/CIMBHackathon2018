b60bbc65a6bd7afdbd74be8ec66d867198d7f101
Han Jun <34719510+hanjun67@users.noreply.github.com>
2018-12-07 17:45:21 +0800

    v1.0

--- cimb.py
+++ cimb.py
@@ -14,7 +14,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-WHICHUSER, BEGINCREATE, SCANFRONTID, SCANBACKID, SCANRESIDENCE = range(5)
+WHICHUSER, BEGINCREATE, SCANFRONTID, SCANBACKID, SCANRESIDENCE, USERNAME, PASSWORD, CHECK = range(8)
 
 start_keyboard =[['Create New Account'],
                  ['Login to Current Account']]
@@ -32,8 +32,38 @@ def new_account(bot, update):
     update.message.reply_text("Thank you for choosing CIMB.\nWe require several pieces of information from you. This will require a camera-enabled device.\n1) National ID\n2) Proof of Residential Address(Telco, Utility)", reply_markup = new_markup)
     return BEGINCREATE
 
-def login(bot, update):
-    update.message.reply_text("fuck off")
+
+######################################################################################
+# For Database query
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tableDefine import *
+
+engine = create_engine('sqlite:///data.db', echo = True)
+Session = sessionmaker(bind = engine)
+session = Session()
+cimb_credentials_database = session.query(Credentials).all()
+def check_credential(user_name_value, password_value, databases):
+    for item in databases:
+        if item.user_name == user_name_value:
+            if item.password == password_value:
+                return 'Login Successful'
+    return 'Invalid Username or Password'
+######################################################################################
+
+def for_username(bot, update):
+    update.message.reply_text("Welcome to CIMB Login Page!")
+    update.message.reply_text("Input Username")
+    return PASSWORD
+
+def for_password(bot, update, user_data):
+    user_data['username'] = update.message.text
+    update.message.reply_text("Input Password")
+    return CHECK
+
+def check(bot, update, user_data):
+    user_data['password'] = update.message.text
+    update.message.reply_text(check_credential(user_data['username'], user_data['password'], cimb_credentials_database))
     return ConversationHandler.END
 
 def scanFrontID(bot, update):
@@ -41,17 +71,34 @@ def scanFrontID(bot, update):
     return SCANFRONTID
 
 def scanBackID(bot, update):
+    file_id = update.message.photo[-1].file_id
+    newFile = bot.getFile(file_id)
+    newFile.download('frontID_' + str(update.message.from_user.id) + '_' + str(update.message.date).replace(':','') + '.jpg')
     update.message.reply_text("Please send a picture of the back of your National ID")
     return SCANBACKID
 
 def scanResidence(bot, update):
+    file_id = update.message.photo[-1].file_id
+    newFile = bot.getFile(file_id)
+    newFile.download('backID_' + str(update.message.from_user.id) + '_' + str(update.message.date).replace(':','') + '.jpg')
     update.message.reply_text("Please send a picture of the proof of residence")
     return SCANRESIDENCE
+
+def received(bot,update):
+    file_id = update.message.photo[-1].file_id
+    newFile = bot.getFile(file_id)
+    newFile.download('residence_' + str(update.message.from_user.id) + '_' + str(update.message.date).replace(':','') + '.jpg')
+    update.message.reply_text("Your documents are received successfully.\n\n Processing may take up to two working days.\n\n You will receive a message from us shortly.")
+    return ConversationHandler.END
     
 def finish(bot,update):
     update.message.reply_text("Bye motherfucker")
     return ConversationHandler.END
     
+def invalid(bot,update):
+    update.message.reply_text("Invalid input")
+    return ConversationHandler.END
+
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
@@ -64,13 +111,15 @@ def main():
     conv_handler = ConversationHandler(
             entry_points = [CommandHandler('start',start)],
             states = {
-                    WHICHUSER: [RegexHandler('^Create New Account$', new_account), RegexHandler('^Login to Current Account$', login)],
+                    WHICHUSER: [RegexHandler('^Create New Account$', new_account), RegexHandler('^Login to Current Account$', for_username)],
                     BEGINCREATE: [RegexHandler('^Continue$', scanFrontID), RegexHandler('^Exit$', finish)],
                     SCANFRONTID: [MessageHandler(Filters.photo, scanBackID)],
                     SCANBACKID: [MessageHandler(Filters.photo, scanResidence)],
-                    SCANRESIDENCE: [MessageHandler(Filters.photo, finish)]
+                    SCANRESIDENCE: [MessageHandler(Filters.photo, received)],
+                    PASSWORD: [MessageHandler(Filters.text, for_password, pass_user_data = True)],
+                    CHECK: [MessageHandler(Filters.text, check, pass_user_data = True)]
                     },
-            fallbacks = [RegexHandler('^Finish$', finish, pass_user_data=True)]
+            fallbacks = [MessageHandler(Filters.text,invalid)]
             
             )
     
